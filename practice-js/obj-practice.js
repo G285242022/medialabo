210928984f9ad3f860274f1ea5783f12272c332f
G285242022 <228524@st.takushoku-u.ac.jp>
2023-06-22 16:35:45 +0900

    更新

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -15,4 +15,13 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
+console.log(campus.address);
 
+for (let element of campus.buildingD) {
+	console.log(element);
+}
+
+for (let element2 of gakka) {
+	console.log(element2);
+	
+}
